3664c3c1e0e48df0f402eea61835d398a959efbd
Psych0d0g <bigluks@gmail.com>
2013-09-30 22:39:02 +0200

    pdroid related changes

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -363,7 +363,7 @@ public class GSMPhone extends PhoneBase {
      * Notify any interested party of a Phone state change
      * {@link com.android.internal.telephony.PhoneConstants.State}
      */
-    /*package*/ void notifyPhoneStateChanged() {
+    /*package*/ public void notifyPhoneStateChanged() {
         mNotifier.notifyPhoneState(this);
     }
 
@@ -402,7 +402,7 @@ public class GSMPhone extends PhoneBase {
     }
 
     /*package*/
-    void notifyLocationChanged() {
+    public void notifyLocationChanged() {
         mNotifier.notifyCellLocation(this);
     }
 
